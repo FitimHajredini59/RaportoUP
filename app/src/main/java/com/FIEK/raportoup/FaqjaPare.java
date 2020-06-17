825bc837a972f8e08212c3bfb970f91b7486a98c
FitimHajredini59 <44604433+FitimHajredini59@users.noreply.github.com>
2020-06-17 18:53:58 +0200

    Pamja e faqes se pare (kryesore)
    Shtimi i elementeve te nevojshme per faqen e pare.

--- app/src/main/java/com/FIEK/raportoup/FaqjaPare.java
+++ app/src/main/java/com/FIEK/raportoup/FaqjaPare.java
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class FaqjaPare extends AppCompatActivity {
+public class FaqjaPare extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     String strExtras = "";
+
+    /*-------------------------E shtuar per spinnerin--------------------------------*/
+    private Spinner spinner;
+    private static final String[] paths = {"profesor", "asistent", "zyrtar referent", "prodekan", "dekan"
+            , "zyrtar të IT", "mbeturina", "dëmtime fizike të objekteve", "të tjera"};
+    /*-------------------------E shtuar per spinnerin--------------------------------*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +30,66 @@ public class FaqjaPare extends AppCompatActivity {
             strExtras = getIntent().getExtras().getString("username");
 
         TextView tv1 = findViewById(R.id.tv1);
-        tv1.setText("Mirë se vini, " + strExtras);
+        tv1.setText("Mirë se vini, " + strExtras + "!");
+
+        /*-------------------------E shtuar per spinnerin--------------------------------*/
+        spinner = (Spinner)findViewById(R.id.kategorite);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(FaqjaPare.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        /*-------------------------E shtuar per spinnerin--------------------------------*/
     }
+
+
+
+    /*-------------------------E shtuar per spinnerin--------------------------------*/
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+            case 3:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+            case 4:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+            case 5:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+            case 6:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+            case 7:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+            case 8:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+    /*-------------------------E shtuar per spinnerin--------------------------------*/
 
     boolean getBack = false;
 
     @Override
     public void onBackPressed() {
-
         if (getBack)
             super.onBackPressed();
 
@@ -49,7 +111,7 @@ public class FaqjaPare extends AppCompatActivity {
             }
         });
         alertDialog.show();
-
-
     }
+
+
 }
