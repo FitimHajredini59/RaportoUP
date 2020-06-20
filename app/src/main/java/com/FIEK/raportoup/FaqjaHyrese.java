5a1bc742d8a901c723ea24b60884560f55a8ea41
Edon Budakova <ebudakova99@gmail.com>
2020-06-20 17:33:27 +0200

    Animation_pamja_hyrese
    nje animation logo, per pamjen e pare te app, kliko ne ekran dhe vazhdo per t'u kyçyr

---
+++ app/src/main/java/com/FIEK/raportoup/FaqjaHyrese.java
@@ -0,0 +1,35 @@
+package com.FIEK.raportoup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FaqjaHyrese extends AppCompatActivity {
+
+
+    private RelativeLayout loginLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_faqja_hyrese);
+        LinearLayout llayout = (LinearLayout) findViewById(R.id.activity_faqja_hyrese);
+        llayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intt= new Intent(FaqjaHyrese.this, LoginActivity.class);
+                startActivity(intt);
+
+            }
+
+            }
+        );
+    }}
+
